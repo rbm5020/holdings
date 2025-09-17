2988284e1873ec2714a0c35c98898e5c7b7f034b
Robert Morris <rbm@Roberts-MacBook-Air.local>
2025-09-16 21:41:35 -0400

    Revert "Add Vercel API endpoints for portfolio functionality"
    This reverts commit 8c3d1228da22c2fea5fbec0b1ee7e8110e0ab596.

--- api/portfolios/[id].js
+++
@@ -1,5 +0,0 @@
-// This will be handled by the main portfolios.js for now
-// Vercel dynamic routes will be implemented as we expand
-export default function handler(req, res) {
-    return res.status(404).json({ error: 'Use /api/portfolios with query params' });
-}
